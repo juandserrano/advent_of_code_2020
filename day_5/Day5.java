d1140e76a05fe4960bda44b006806af712e6957e
Juan Diego Serrano <juandserrano@gmail.com>
2021-04-17 17:21:26 -0400

    done day 5 part one

--- day_5/Day5.java
+++ day_5/Day5.java
@@ -1,12 +1,51 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Day5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         final int FIRST_ROW = 0;
         final int LAST_ROW = 127;
-    
-        String positioning = "FFFFFFFRRR";
-        int row = SeatCalculator.searchRow(FIRST_ROW, LAST_ROW, positioning);
-        System.out.println(row);
+        int highestSeatId = 0;
+
+        List<String> mainArray = new ArrayList<String>();
+        
+        try {
+            Path filePath = Path.of("./day_5/input.txt");
+            String data = Files.readString(filePath);
+            String regex = "([FBLR]{10})";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(data);
+            while (m.find()){
+                mainArray.add(m.group());
+            }
+           
+            for (String ticket : mainArray) {
+                //String positioning = ticket;
+                int row = SeatCalculator.searchRow(FIRST_ROW, LAST_ROW, ticket);
+                int column = SeatCalculator.searchColumn(ticket);
+                int id = row * 8 + column;
+                System.out.println(row);
+                System.out.println(column);
+                System.out.println(id);
+                if (id > highestSeatId){
+                    highestSeatId = id;
+                }
+                
+            }
+            
+            
+            System.out.printf("The highest seat ID is: %d", highestSeatId);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     
--- day_5/SeatCalculator.java
+++ day_5/SeatCalculator.java
@@ -10,15 +10,27 @@ public class SeatCalculator {
             
             
                 if (positioning.charAt(i) == 'F'){
-                    temp_array[1] = temp_array[1] - ((temp_array[1] - temp_array[0])/2);
+                    temp_array[1] = temp_array[1] - ((temp_array[1] - temp_array[0])/2) - 1;
                 } else if (positioning.charAt(i) == 'B'){
-                    temp_array[0] = temp_array[0] + ((temp_array[1] - temp_array[0])/2);
+                    temp_array[0] = temp_array[0] + ((temp_array[1] - temp_array[0])/2) + 1;
                 } 
-            System.out.println(positioning.charAt(i));
-            System.out.println(temp_array[0]);
-            System.out.println(temp_array[1]);
+         
         }
         return temp_array[1];
+        
+    }
+
+    public static int searchColumn(String positioning) {
+        int[] cols = {0 ,7};
+        for (int i = 7; i < positioning.length(); i++){
+                if (positioning.charAt(i) == 'L'){
+                    cols[1] = cols[1] - ((cols[1] - cols[0])/2) - 1;
+                } else if (positioning.charAt(i) == 'R'){
+                    cols[0] = cols[0] + ((cols[1] - cols[0])/2) + 1;
+                } 
+            
+        }
+        return cols[1];
         
     }
 }
