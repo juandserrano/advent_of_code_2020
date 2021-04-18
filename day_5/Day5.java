ccde8cf7846ed374d1e62059a70cc080ca90dd9a
Juan Diego Serrano <juandserrano@gmail.com>
2021-04-18 17:23:55 -0400

    day 5 done

--- day_5/Day5.java
+++ day_5/Day5.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,7 +14,11 @@ public class Day5 {
         final int LAST_ROW = 127;
         int highestSeatId = 0;
 
-        List<String> mainArray = new ArrayList<String>();
+       // List<String> mainArray = new ArrayList<String>();
+        List<Integer> occupiedRows = new ArrayList<Integer>();
+        List<Integer> possibleIds = new ArrayList<Integer>();
+
+        
         
         try {
             Path filePath = Path.of("./day_5/input.txt");
@@ -22,25 +27,31 @@ public class Day5 {
             Pattern p = Pattern.compile(regex);
             Matcher m = p.matcher(data);
             while (m.find()){
-                mainArray.add(m.group());
-            }
-           
-            for (String ticket : mainArray) {
-                //String positioning = ticket;
+                
+                String ticket = m.group();
                 int row = SeatCalculator.searchRow(FIRST_ROW, LAST_ROW, ticket);
                 int column = SeatCalculator.searchColumn(ticket);
                 int id = row * 8 + column;
-                System.out.println(row);
-                System.out.println(column);
-                System.out.println(id);
+                
+                occupiedRows.add(id);
                 if (id > highestSeatId){
                     highestSeatId = id;
                 }
-                
+
             }
-            
+           
             
             System.out.printf("The highest seat ID is: %d", highestSeatId);
+            Collections.sort(occupiedRows);
+            System.out.println(occupiedRows);
+
+            for (int i = 70; i <= 826; i++){
+                if(!occupiedRows.contains(i)){
+                    System.out.println(i);
+                }
+            }
+
+           
             
         } catch (Exception e) {
             e.printStackTrace();
